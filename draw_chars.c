eceda9ac2d3ba70338acf5ff05f1c40cf50b0af7
SeanBLaNeave <seanlaneave@gmail.com>
2024-06-16 14:05:56 -0600

    adding in the big ascii letters

--- draw_chars.c
+++ draw_chars.c
@@ -4,10 +4,24 @@
 void print_char_5x7(char c)
 {
   c -= 0x20;
-  for (char col = 0; col < 5; col++) {
-    for (char row = 0; row < 7; row++) {
+  for (char row = 6; row >= 0; row--) {
+    unsigned short colMask = 1 << (6-row);
+    for (char col = 0; col < 5; col++) {
       unsigned short rowBits = font_5x7[c][col];
-      unsigned short colMask = 1 << (6-row); /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
+
+void print_char_11x16(char c)
+{
+  c -= 0x20;
+  for (char row = 15; row >= 0; row--) {
+    unsigned short colMask = 1 << (15-row);
+    for (char col = 0; col < 11; col++) {
+      unsigned short rowBits = font_11x16[c][col];
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
